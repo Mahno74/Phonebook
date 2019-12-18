b8d2fa26bb1ec94396536d8487091e9325b5eb4b
Юрасов Вячеслав <2665603@gmail.com>
2019-12-18 14:08:17 +0500

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Phonebook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox_FIO = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_Podrazdelenie = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label4 = new System.Windows.Forms.Label();
+            this.maskedTextBox_Telefon = new System.Windows.Forms.MaskedTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 90);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(1041, 263);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.DataGridView1_CellMouseDoubleClick);
+            // 
+            // textBox_FIO
+            // 
+            this.textBox_FIO.Location = new System.Drawing.Point(22, 45);
+            this.textBox_FIO.Name = "textBox_FIO";
+            this.textBox_FIO.Size = new System.Drawing.Size(203, 20);
+            this.textBox_FIO.TabIndex = 1;
+            this.textBox_FIO.TextChanged += new System.EventHandler(this.TextBox_FIOTextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(595, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 24);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Сбросить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(19, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Фамилия";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // textBox_Podrazdelenie
+            // 
+            this.textBox_Podrazdelenie.Location = new System.Drawing.Point(231, 46);
+            this.textBox_Podrazdelenie.Name = "textBox_Podrazdelenie";
+            this.textBox_Podrazdelenie.Size = new System.Drawing.Size(269, 20);
+            this.textBox_Podrazdelenie.TabIndex = 2;
+            this.textBox_Podrazdelenie.TextChanged += new System.EventHandler(this.TextBox_PodrazdelenieTextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(228, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Подразделение";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(722, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(342, 39);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "*при нажатии на выделенные цветом ячейки в таблице \r\nпроизводится поиск  сотрудни" +
+    "ков по номеру кабинета \r\nили телефона.";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.оПрограммеToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1066, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(94, 20);
+            this.оПрограммеToolStripMenuItem.Text = "О программе";
+            this.оПрограммеToolStripMenuItem.Click += new System.EventHandler(this.ОПрограммеToolStripMenuItem_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(511, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Тел.";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // maskedTextBox_Telefon
+            // 
+            this.maskedTextBox_Telefon.Location = new System.Drawing.Point(514, 46);
+            this.maskedTextBox_Telefon.Mask = "000-00-00";
+            this.maskedTextBox_Telefon.Name = "maskedTextBox_Telefon";
+            this.maskedTextBox_Telefon.Size = new System.Drawing.Size(62, 20);
+            this.maskedTextBox_Telefon.TabIndex = 3;
+            this.maskedTextBox_Telefon.TextChanged += new System.EventHandler(this.MaskedTextBox_Telefon_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1066, 365);
+            this.Controls.Add(this.maskedTextBox_Telefon);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_Podrazdelenie);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox_FIO);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox_FIO;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_Podrazdelenie;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox_Telefon;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Phonebook
+{
+    public partial class Form1 : Form
+    {
+        //public DataTable Table;
+        public string Path = "Provider=Microsoft.Jet.OLEDB.4.0; Data Source=SPRAV.mdb";
+
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Программа для работы с базой телефонных номеров Администрации города Челябинска v 2.0";
+            dataGridView1.DataSource = GetTable();
+            dataGridView1.DefaultCellStyle.Font = new Font("Times New Roman", 9F);
+            dataGridView1.DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+            dataGridView1.Columns[1].DefaultCellStyle.BackColor = Color.LightCyan;
+            dataGridView1.Columns[0].DefaultCellStyle.Font = new Font("Times New Roman", 9F, FontStyle.Bold);
+            dataGridView1.Columns[2].DefaultCellStyle.BackColor = Color.LightCyan;
+            dataGridView1.Columns[4].DefaultCellStyle.BackColor = Color.LightCyan;
+            InputLanguage.CurrentInputLanguage = InputLanguage.FromCulture(System.Globalization.CultureInfo.CreateSpecificCulture("Ru"));
+            dataGridView1.Columns[0].Width = 220;
+            dataGridView1.Columns[1].Width = 120;
+            dataGridView1.Columns[2].Width = 60;
+
+            textBox_FIO.Select(); //установка курсора в строку поиска
+
+        }
+        private async void TextBox_FIOTextChanged(object sender, EventArgs e) //поиск по фамилии
+        {
+            textBox_Podrazdelenie.Clear(); maskedTextBox_Telefon.Clear();
+            if (textBox_FIO.Text.Length < 2) return;  //не ищем если знаков меньше 2
+            string request = "SELECT Люди.Фамилия, Люди.Имя, Люди.Отчество, IIf([НетТелефон],\"\",[Телефон]) AS Тел1, IIf([НетКабинет],\"\",[Кабинет]) AS Каб, Должности.Должность, Подразделения.Подразделения1, Структура.Адрес " +
+    "FROM (Стили RIGHT JOIN (Подразделения INNER JOIN Структура ON Подразделения.Код_подразделения = Структура.КодПодразделения) ON Стили.Стиль = Структура.Стиль) INNER JOIN ((Люди LEFT JOIN Должности ON Люди.КодДолжности = Должности.Код_должности) INNER JOIN Справочник ON Люди.код_Сотрудника = Справочник.Код_сотрудника) ON Структура.КодСтруктуры = Справочник.КодСтруктуры " +
+    "WHERE [Люди.Фамилия] Like '" + textBox_FIO.Text + "%' order by [Люди.Имя]";
+            DataTable tab = new DataTable();
+            await Task.Run(() => tab = GetTable(request));
+            dataGridView1.DataSource = tab;
+            dataGridView1[0, 0].Selected = false;
+        }
+        private async void TextBox_PodrazdelenieTextChanged(object sender, EventArgs e) //поиск по организации
+        {
+
+            textBox_FIO.Clear(); maskedTextBox_Telefon.Clear();
+            if (textBox_Podrazdelenie.Text.Length < 4) return;   //не ищем если знаков меньше 2
+            string request = "SELECT Люди.Фамилия, Люди.Имя, Люди.Отчество, IIf([НетТелефон],\"\",[Телефон]) AS Тел1, IIf([НетКабинет],\"\",[Кабинет]) AS Каб, " +
+    "Должности.Должность, Подразделения.Подразделения1, Структура.Адрес FROM (Стили RIGHT JOIN (Подразделения INNER JOIN Структура ON Подразделения.Код_подразделения = Структура.КодПодразделения) ON Стили.Стиль = Структура.Стиль) INNER JOIN ((Люди LEFT JOIN Должности ON Люди.КодДолжности = Должности.Код_должности) INNER JOIN Справочник ON Люди.код_Сотрудника = Справочник.Код_сотрудника) ON Структура.КодСтруктуры = Справочник.КодСтруктуры " +
+    "WHERE [Подразделения.Подразделения1] Like '%" + textBox_Podrazdelenie.Text + "%' order by [Фамилия]";
+            DataTable tab = new DataTable();
+            await Task.Run(() => tab = GetTable(request));
+            dataGridView1.DataSource = tab;
+            dataGridView1[0, 0].Selected = false;
+
+        }
+        private async void MaskedTextBox_Telefon_TextChanged(object sender, EventArgs e) //поиск по номеру телефона
+        {
+            textBox_FIO.Clear(); textBox_Podrazdelenie.Clear();
+            if (maskedTextBox_Telefon.Text.Length < 3) return;  //не ищем если знаков меньше 2
+            string Request = "SELECT Люди.Фамилия, Люди.Имя, Люди.Отчество, IIf([НетТелефон],\"\",[Телефон]) AS Тел1, IIf([НетКабинет],\"\",[Кабинет]) AS Каб, Должности.Должность, Подразделения.Подразделения1, Структура.Адрес " +
+                    "FROM (Стили RIGHT JOIN (Подразделения INNER JOIN Структура ON Подразделения.Код_подразделения = Структура.КодПодразделения) ON Стили.Стиль = Структура.Стиль) INNER JOIN ((Люди LEFT JOIN Должности ON Люди.КодДолжности = Должности.Код_должности) INNER JOIN Справочник ON Люди.код_Сотрудника = Справочник.Код_сотрудника) ON Структура.КодСтруктуры = Справочник.КодСтруктуры " +
+                    "WHERE [Телефон] Like '" + maskedTextBox_Telefon.Text + "%' order by [Фамилия]";
+            DataTable tab = new DataTable();
+            await Task.Run(() => tab = GetTable(Request));
+            dataGridView1.DataSource = tab;
+            dataGridView1[0, 0].Selected = false;
+        }
+
+
+        private DataTable GetTable(string request = "")  //выдаем таблицу с результатами поиска
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("ФИО");       // 0
+            table.Columns.Add("Телефон"); // 1
+            table.Columns.Add("Кабинет");     // 2
+            table.Columns.Add("Должность");       // 3
+            table.Columns.Add("Подразделение");       // 4
+            table.Columns.Add("Адрес");      // 5
+            if (request == "") return table;
+            OleDbConnection Connection = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0; Data Source=SPRAV.mdb");
+            Connection.Open();
+            OleDbCommand Command = new OleDbCommand(request, Connection);
+            OleDbDataReader Reader = Command.ExecuteReader(); ;
+
+            while (Reader.Read() == true)
+            { // Заполнение Клеток таблицы
+
+                table.Rows.Add(
+                    Reader.GetValue(0).ToString().ToUpper() + "\n" + Reader.GetValue(1).ToString() + " " + Reader.GetValue(2).ToString(),  //Фамилия Имя Отчество
+                    Reader.GetValue(3),
+                    Reader.GetValue(4),
+                    Reader.GetValue(5),
+                    Reader.GetValue(6),
+                    Reader.GetValue(7)
+                               );
+            }
+            Command.Dispose(); Connection.Close();
+            return table;
+        }
+
+        private void DataGridView1_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            string selectedCell = dataGridView1.CurrentCell.Value.ToString().Trim();
+            //string selectedColl = dataGridView1.CurrentCellAddress.ToString();
+            string selectedColl = dataGridView1.Columns[dataGridView1.CurrentCell.ColumnIndex].HeaderText.ToString(); //получение заголовка столбца
+            if (selectedColl == "Кабинет" || selectedColl == "Телефон" || selectedColl == "Подразделение")
+            {
+                if (selectedColl == "Подразделение") selectedColl = "Подразделения1";
+                string Request = "SELECT Люди.Фамилия, Люди.Имя, Люди.Отчество, IIf([НетТелефон],\"\",[Телефон]) AS Тел1, IIf([НетКабинет],\"\",[Кабинет]) AS Каб, Должности.Должность, Подразделения.Подразделения1, Структура.Адрес " +
+        "FROM (Стили RIGHT JOIN (Подразделения INNER JOIN Структура ON Подразделения.Код_подразделения = Структура.КодПодразделения) ON Стили.Стиль = Структура.Стиль) INNER JOIN ((Люди LEFT JOIN Должности ON Люди.КодДолжности = Должности.Код_должности) INNER JOIN Справочник ON Люди.код_Сотрудника = Справочник.Код_сотрудника) ON Структура.КодСтруктуры = Справочник.КодСтруктуры " +
+        "WHERE [" + selectedColl + "] Like '" + selectedCell + "' order by [Фамилия]";
+
+                dataGridView1.DataSource = GetTable(Request);
+            }
+        }
+        private void Button1_Click(object sender, EventArgs e) //отчистка
+        {
+            textBox_FIO.Clear(); textBox_Podrazdelenie.Clear(); maskedTextBox_Telefon.Clear();
+            dataGridView1.DataSource = GetTable();
+            label3.Visible = false;
+        }
+        private void ОПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Программа для работы с базой телефонных номеров\nАдминистрации города Челябинска" + "\n© Юрасов Вячеслав Викторович\n+7 (919) 113-24-13\n \n2665603@gmail.com");
+        }
+    }
+}
